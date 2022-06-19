1603c8dcd2fb73631fe779ff34d801939d65431c
Dmytro Kravchenko <dmitrij.kravchenko.1997@gmail.com>
2022-06-19 16:56:22 +0300

    ver. 2.1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,12 +5,12 @@
 // Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів.
 // Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною(disabled).
 
-const body = document.querySelector("body")
+const body = document.querySelector("body");
 const startBtn = document.querySelector("[data-start]");
 const stopBtn = document.querySelector("[data-stop]");
 
 startBtn.addEventListener("click", onChangeColorStart);
-stopBtn.addEventListener("click", onChangeColorStop)
+stopBtn.addEventListener("click", onChangeColorStop);
 
 function onChangeColorStart() {
     stopBtn.removeAttribute("disabled");
@@ -19,14 +19,14 @@ function onChangeColorStart() {
     intervalId = setInterval(() => {
         body.style.backgroundColor = getRandomHexColor();
     }, 1000);
-}
+};
 
 function onChangeColorStop() {
     startBtn.removeAttribute("disabled");
     stopBtn.setAttribute("disabled", "disabled");
     clearInterval(intervalId);
-}
+};
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
